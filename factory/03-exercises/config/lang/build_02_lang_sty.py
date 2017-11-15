af8fe9c431e5fa8be51aa418130c91e4d5a9b352
Christophe BAL <projetmbc@gmail.com>
2017-11-15 14:43:54 +0100

    Short names for contexts

--- factory/03-exercises/config/lang/build_02_lang_sty.py
+++ factory/03-exercises/config/lang/build_02_lang_sty.py
@@ -12,12 +12,20 @@ THIS_DIR = PPath(__file__).parent
 STY_HEADER = withframe(text = "CONTEXTS", frame = ALL_FRAMES["latex_pretty"])
 
 
-# --------------- #
-# -- CONSTANTS -- #
-# --------------- #
+# ----------- #
+# -- TOOLS -- #
+# ----------- #
 
-def normalize(translation):
-    return translation
+def longshortversions(defs):
+    longversion, *shortversion = defs.split("|")
+
+    if shortversion:
+        shortversion = "|".join(shortversion)
+
+    else:
+        shortversion = longversion
+
+    return ("", longversion.strip()), ("short", shortversion.strip())
 
 
 # -------------------------- #
@@ -33,10 +41,10 @@ for ppath in THIS_DIR.walk("file::*.peuf"):
     ) as data:
         for _, transtalions in data.mydict("mini std").items():
             for key, val in transtalions.items():
-                content.append(
-                    f"\\newcommand\\lyxam@text@{key}{{{val}}}"
-                )
-
+                for kind, name in longshortversions(val):
+                    content.append(
+                        f"\\newcommand\\lyxam@{kind}text@{key}{{{name}}}"
+                    )
 
     with ppath.with_ext("sty").open(
         mode     = "w",
